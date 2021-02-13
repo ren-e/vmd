707b436f485b929e00d8d404d6f6393efd904799
ren-e <0xff@qsrs.nl>
2021-02-13 16:10:07 +0100

    Improve VM state handling and reporting

--- vm.m
+++ vm.m
@@ -32,9 +32,48 @@
 
 static int abortsignal = 0;
 
-static void abort_trap() {
+static void
+abort_trap()
+{
 	if (abortsignal < HARD_ABORT_CTR)
 		abortsignal += 1;
+}
+
+static void
+log_vm_state(char *name, int state)
+{
+	if (abortsignal == HARD_ABORT_CTR)
+		state = VZVirtualMachineStateStopped;
+
+	switch (state) {
+	case VZVirtualMachineStateStopped:
+		if (abortsignal == HARD_ABORT_CTR)
+			NSLog(@"VM %s has forcefully shutdown", name);
+		else
+			NSLog(@"VM %s has shutdown", name);
+		break;
+	case VZVirtualMachineStateRunning:
+		NSLog(@"VM %s has started", name);
+		break;
+	case VZVirtualMachineStatePaused:
+		NSLog(@"VM %s has paused", name);
+		break;
+	case VZVirtualMachineStateError:
+		NSLog(@"VM %s has encountered an internal error", name);
+		break;
+	case VZVirtualMachineStateStarting:
+		NSLog(@"VM %s is configuring the hardware and starting", name);
+		break;
+	case VZVirtualMachineStatePausing:
+		NSLog(@"VM %s is being paused", name);
+		break;
+	case VZVirtualMachineStateResuming:
+		NSLog(@"VM %s is being resumed", name);
+		break;
+	default:
+		NSLog(@"VM %s in unknown state %d", name, state);
+		break;
+	}
 }
 
 int
@@ -105,7 +144,6 @@ run_vm(struct parse_result *res, struct vmconfig *vmcfg)
 	/* Trap CTRL+C */
 	signal(SIGINT, abort_trap);
 
-	/* TODO: Should trap signals here */
 	dispatch_queue_t q = dispatch_queue_create(NULL, NULL);
         
         VZVirtualMachine *vm = [
@@ -116,19 +154,17 @@ run_vm(struct parse_result *res, struct vmconfig *vmcfg)
 
 	/* VM must be tested and started inside the queue */
 	dispatch_sync(q, ^{
-	  if (!vm.canStart) {
-		NSLog(@"Failed to start VM");
-		exit(1);
-	  }
+	  if (!vm.canStart)
+		NSLog(@"Unable to start VM");
+
 	  [vm startWithCompletionHandler:^(NSError * _Nullable errorOrNil) {
-	  if (errorOrNil) {
+	  if (errorOrNil)
 		NSLog(@"Failed to start VM: %@", errorOrNil);
-		exit(1);
-	  }
 	  }];
 	});
 
-	NSLog(@"Succesfully started VM %s", vmcfg->name);
+	if (verbose > 1)
+		log_vm_state(vmcfg->name, vm.state);
 
 	/* Spawn console on child */
 	if (res->tty_autoconnect == 1) {
@@ -137,24 +173,31 @@ run_vm(struct parse_result *res, struct vmconfig *vmcfg)
 			ctl_openconsole(vmcfg);
 	}
 
+	/* Wait for final VM state */
+	while (vm.state == VZVirtualMachineStateStarting && abortsignal == 0) {
+		usleep(100);
+	}
+
+	if (vm.state != VZVirtualMachineStateRunning)
+		goto done;
+
+	log_vm_state(vmcfg->name, vm.state);
+
 	/* Keepalive */
-	while (
-	  (vm.state == VZVirtualMachineStateStarting ||
-	   vm.state == VZVirtualMachineStateRunning) &&
-	  abortsignal == 0
-	) {
+	while (vm.state == VZVirtualMachineStateRunning && abortsignal == 0) {
 		sleep(1);
 	}
 
 	if (abortsignal)
 		stop_vm(&q, vm);
 
+done:
 	if (res->tty_autoconnect == 1 && pid != -1)
 		kill(pid, SIGINT);
 
 	/* Wait for child to terminate */
 	waitpid(pid, 0, 0);
-	NSLog(@"VM %s has shutdown with state %ld", vmcfg->name, vm.state);
+	log_vm_state(vmcfg->name, vm.state);
 
 	if (vm)
 		[vm release];
